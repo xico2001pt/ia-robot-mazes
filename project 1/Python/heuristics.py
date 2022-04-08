bb3435bf1d2f6feb286e4cc0fc6eb98afcd0e649
Filipe Campos <filipepcampos24@gmail.com>
2022-04-08 15:21:19 +0100

    LTP seems to be working with BFS

--- project 1/Python/heuristics.py
+++ project 1/Python/heuristics.py
@@ -1,9 +1,10 @@
 from solver import State, SearchProblemSolver
 
 class LTPState(State):
-    def __init__(self, x, y, turns=[]):
+    def __init__(self, x, y, current_direction='', turns=[]):
         self.x = x
         self.y = y
+        self.current_direction = current_direction
         self.turns = turns
 
     def get_number_of_turns(self):
@@ -18,12 +19,43 @@ class LTPState(State):
         return False
 
 class LTPSolver(SearchProblemSolver):
+    def __init__(self, initial_state, maze):
+        super().__init__(initial_state)
+        self.maze = maze
+
     def cost(self, state):
         return state.get_number_of_turns()
     
-    def operators(self, state):
+    def heuristic(self, state):
         raise NotImplementedError()
 
+    def operators(self, state):
+        new_states = []
+        for pos in self.maze.adjacencies[(state.x, state.y)]:
+            delta_x = pos[0] - state.x
+            delta_y = pos[1] - state.y
+            direction = ''
+            if(delta_x == 0):
+                direction = 'U' if delta_y < 0 else 'D'
+            elif(delta_y == 0):
+                direction = 'R' if delta_x > 0 else 'L'
+            else:
+                raise "TODO: this shouldn't happen"
+
+            turns = state.turns.copy()
+            if direction != state.current_direction:
+                turns += [direction]
+            new_states.append(LTPState(pos[0], pos[1], direction, turns))
+        return new_states
+
     def is_final_state(self, state):
-        raise NotImplementedError()
-    
+        return state.x == 3 and state.y == 0
+
+if __name__ == "__main__":
+    from maze import Maze
+    initial_state = LTPState(0, 3)
+    maze = Maze()
+    maze.open_from_file('example_maze.txt')
+    solver = LTPSolver(initial_state, maze)
+    solution = solver.breath_first_search(50)
+    print('\n'.join(str(i) for i in solution[0]))
