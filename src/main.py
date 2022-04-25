8dded7f4ea8ca79ada4abd928b1196740f603f31
adr1an0s0ar3s <adrianosoares35@gmail.com>
2022-04-25 14:39:03 +0100

    :pencil2: Cleaned up main()

--- src/main.py
+++ src/main.py
@@ -1,15 +1,9 @@
 from loop import GameLoop
 from graphics import PygameScreenGUI
 from state import MainMenuState
-from model import MainMenu, Game
-from viewer import GameViewer
 
 def main():
-    from maze import Maze
-
-    m = Maze("../assets/mazes/maze06.txt")
-    print(GameViewer(Game(m)).get_size())
-    game_gui = PygameScreenGUI(500, 500) #TODO: This is absolutely stupid
+    game_gui = PygameScreenGUI(500, 500)
     game_loop = GameLoop(30, game_gui, MainMenuState())
     game_loop.run()
     game_gui.close()
